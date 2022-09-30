80dba069f226493fbb0ba273a0f9f342d8bc7a53
Seven Killer <109410892+SevenKiller2022@users.noreply.github.com>
2022-09-30 18:44:05 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+<script>
+  var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop:true,
+});
+  </script>
